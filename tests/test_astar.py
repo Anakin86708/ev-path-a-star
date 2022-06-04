385c7b94519b8c2c538379ef4c9fc4affe0de046
Ariel Tadeu da Silva <silva.ariel2013@gmail.com>
2022-06-04 15:47:08 -0300

    WIP - Incrementing A* algorithm.
    Signed-off-by: Ariel Tadeu da Silva <silva.ariel2013@gmail.com>

--- tests/test_astar.py
+++ tests/test_astar.py
@@ -14,7 +14,74 @@ class TestAStar(TestCase):
         self.assertIsInstance(instance.graph, Graph)
 
     def test_path_from_to(self):
-        self.fail()
+        # TODO: replace with real graph from util.py
+        e = [
+            ('1', '2', 5),
+            ('2', '1', 5),
+            ('1', '3', 5),
+            ('3', '1', 5),
+            ('2', '3', 5),
+            ('3', '2', 5),
+            ('3', '11', 5),
+            ('11', '3', 5),
+            ('4', '3', 4),
+            ('3', '4', 4),
+            ('4', '10', 2),
+            ('10', '4', 2),
+            ('5', '2', 5),
+            ('2', '5', 5),
+            ('5', '3', 4),
+            ('3', '5', 4),
+        ]
+        expected = [
+            ('Arad', 'Sibiu'),
+            ('Sibiu', 'Rimnicu Vilcea'),
+            ('Rimnicu Vilcea', 'Pitesti'),
+            ('Pitesti', 'Bucharest')
+        ]  # TODO: add expected path
+        graph = Graph(e)
+
+        instance = AStar(graph, lambda x, y: 0)
+        result = instance.path_from_to('5', '10')
+
+        self.assertIsInstance(result, list)
+
+    def test_real_cost(self):
+        mock_graph = Mock()
+        mock_graph.weights = {
+            ('5', '3'): 4,
+            ('3', '4'): 2,
+            ('4', '10'): 3
+        }
+        path = {
+            '5': None,
+            '3': '5',
+            '4': '3',
+            '10': '4'
+        }
+        start_node = '5'
+        end_node = '10'
+        expected = 9
+
+        instance = AStar(mock_graph, lambda x, y: 0)
+        result = instance.real_cost(start_node, end_node, path)
+
+        self.assertEqual(result, expected)
+
+    def test_recreate_path_to_start(self):
+        path = {
+            '5': None,
+            '3': '5',
+            '4': '3',
+            '10': '4'
+        }
+        start_node = '5'
+        end_node = '10'
+        expected = ['5', '3', '4', '10']
+
+        result = AStar.recreate_path_to_start(start_node, end_node, path)
+
+        self.assertEqual(result, expected)
 
     def test_are_nodes_present_on_graph_valid(self):
         start_node = 'x'
