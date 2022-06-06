e9cd6a963e8fc8684f07b77db7baab4587b3ca7b
Leonardo <leeo_skw@hotmail.com>
2022-06-06 13:50:28 -0300

    added documentation

--- ev_path/astar.py
+++ ev_path/astar.py
@@ -13,6 +13,9 @@ class AStar:
 
     @property
     def graph(self):
+        """
+        :return: Returns a graph's copy.
+        """
         return deepcopy(self._graph)
 
     def path_from_to(self, start_node: str, end_node: str) -> List[str]:
@@ -25,7 +28,6 @@ class AStar:
         closed = set()
 
         graph_matrix_adj = self.graph.matrix_adj
-
         while opened.qsize() > 0:
             _, current_node, previous = opened.get()  # acts like pop()
             came_from[current_node] = previous
@@ -36,23 +38,13 @@ class AStar:
 
             possible_nodes = set(graph_matrix_adj.index[graph_matrix_adj.loc[current_node] != 0]).difference(
                 set(closed))
+            
             for connected_node in possible_nodes:
                 cost_node = self.avaliation_function(start_node, current_node, connected_node, came_from)
                 opened.put((cost_node, connected_node, current_node))
-
         return self.recreate_path_to_start(start_node, end_node, came_from)
 
     def avaliation_function(self, start_node, current_node, connected_node, path):
-        """
-
-        :param start_node:
-        :type start_node: str
-        :param current_node:
-        :param connected_node:
-        :param path:
-        :return:
-        :rtype: float
-        """
         return self.heuristic_func(current_node, connected_node) + self.real_cost(start_node, current_node, path)
 
     def real_cost(self, start_node, current_node, path):
@@ -60,7 +52,14 @@ class AStar:
         return sum([self.graph.weights[(n1, n2)] for n1, n2 in zip(path_to_start, path_to_start[1:])])
 
     @staticmethod
-    def recreate_path_to_start(start_node, current_node, path: Dict):
+    def recreate_path_to_start(start_node, current_node, path: Dict) -> List[str]:
+        """
+
+        :param start_node: Graph's starter node.
+        :param current_node: Graph's current node.
+        :param path: Maps the actual previous and the actual node. #HELP ARIEL
+        :return: Returns a List containing the path. #HELP ARIEL
+        """
         path_list = [current_node]
         node = current_node
         while node != start_node:
@@ -69,7 +68,17 @@ class AStar:
         path_list.reverse()
         return path_list
 
-    def are_nodes_present_on_graph(self, start_node, end_node):
+    def are_nodes_present_on_graph(self, start_node, end_node) -> bool:
+        """
+
+        :param start_node: Graph's starter node.
+        :param end_node: Graph's goal node.
+        :return: Returns an exception if the node is not in the graph or a True value if it is.
+        :rtype: Boolean
+        :raise:
+            ValueError: explicacao
+        """
         if start_node not in self.graph.nodes or end_node not in self.graph.nodes:
             raise ValueError(f"Node {start_node if start_node not in self.graph.nodes else end_node} not in graph")
         return True
+
--- ev_path/graph.py
+++ ev_path/graph.py
@@ -12,18 +12,30 @@ class Graph:
 
     @property
     def nodes(self) -> Set[str]:
+        """
+        :return: Returns a Set of unique edges.
+        """
         return Graph.extract_nodes(self.edges)
 
     @property
     def edges(self) -> List[Tuple[str, str]]:
+        """
+        :return:  Returns a copy of edges.
+        """
         return deepcopy(self._edges)
 
     @property
     def weights(self) -> Dict[Tuple[str, str], float]:
+        """
+        :return: Returns a copy of weights.
+        """
         return deepcopy(self._weights)
 
     @property
     def matrix_adj(self) -> pd.DataFrame:
+        """
+        :return: Returns a matrix (dataframe) containing the weight between two nodes.
+        """
         m = len(self.nodes)
         matrix = pd.DataFrame(np.zeros((m, m)), columns=self.nodes, index=self.nodes)
         for (node_1, node_2), cost in zip(self.edges, self._weights.values()):
@@ -32,13 +44,25 @@ class Graph:
 
     @staticmethod
     def extract_nodes(edges: List[Tuple[str, str]]) -> Set[str]:
+        """
+        :param edges: List of tuples with all edges.
+        :return: Returns a Set of unique edges.
+        """
         a, b = zip(*edges)
         return set(a).union(set(b))
 
     @staticmethod
     def extract_weights(edges) -> Dict[Tuple[str, str], float]:
+        """
+        :param edges: List with all edges and its weights.
+        :return: Returns a Dictionary with edges as key and weight as value. I.e. {(2, 5): 10}
+        """
         return {x[:-1]: x[-1] for x in edges}
 
     @staticmethod
     def extract_edges(edges: List[Tuple[str, str, float]]) -> List[Tuple[str, str]]:
+        """
+        :param edges: List with all edges and its weights.
+        :return: Returns a list with all edges.
+        """
         return [x[:-1] for x in edges]
